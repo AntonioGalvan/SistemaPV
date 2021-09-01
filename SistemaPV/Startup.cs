ff8ceb16c1f302b266167d76a8acd6ddf9db32b6
AntonioGalvan <62713228+AntonioGalvan@users.noreply.github.com>
2021-09-01 16:31:41 -0500

    AddIdentity en startup y frameworks en version 3.1.15 instalados

--- SistemaPV/Startup.cs
+++ SistemaPV/Startup.cs
@@ -1,9 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SistemaPV.Data;
+using SistemaPV.Data.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +27,29 @@ namespace SistemaPV
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentity<User, IdentityRole>(cfg =>
+
+            {
+
+                cfg.User.RequireUniqueEmail = true;
+
+                cfg.Password.RequireDigit = false;
+
+                cfg.Password.RequiredUniqueChars = 0;
+
+                cfg.Password.RequireLowercase = false;
+
+                cfg.Password.RequireNonAlphanumeric = false;
+
+                cfg.Password.RequireUppercase = false;
+
+            })
+            .AddEntityFrameworkStores<DataContext>();
+            services.AddDbContext<DataContext>(
+                cfg =>
+                {
+                    cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+                });
             services.AddControllersWithViews();
         }
 
