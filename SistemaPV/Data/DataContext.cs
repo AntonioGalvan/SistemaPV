6794d95224d187d4531082a05a226e07ede6c425
AntonioGalvan <62713228+AntonioGalvan@users.noreply.github.com>
2021-09-01 15:37:05 -0500

    Data context, user y framework 1

---
+++ SistemaPV/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SistemaPV.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaPV.Data
+{
+    public class DataContext : IdentityDbContext<User>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+
+        {
+
+        }
+    }
+}
---
+++ SistemaPV/Data/Entities/User.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaPV.Data.Entities
+{
+    public class User : IdentityUser
+    {
+        [Required(ErrorMessage = "{0} es obligatorio.")]
+
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+
+        [Display(Name = "Nombre")]
+
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "{0} es obligatorio.")]
+
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+
+        [Display(Name = "Apellidos")]
+
+        public string LastName { get; set; }
+
+        [Display(Name = "Teléfono")]
+
+        public override string PhoneNumber { get; set; }
+
+        [Display(Name = "Nombre")]
+
+        public string FullName => $"{LastName} {FirstName}";
+    }
+}
