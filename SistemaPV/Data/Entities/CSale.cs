dabb619674bfb6c2046f6ddd05dbd760e329bb71
Rodolfo0 <fuzzyrodo@gmail.com>
2021-10-03 17:21:06 -0500

    Ventas MVC

--- SistemaPV/Data/Entities/CSale.cs
+++ SistemaPV/Data/Entities/CSale.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,13 +8,35 @@ namespace SistemaPV.Data.Entities
 {
     public class CSale:IEntity
     {
+        [Display(Name = "Id")]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El {0} es requerido.")]
+        [MaxLength(20, ErrorMessage = "Debe introducir un máximo de {1} caracteres.")]
+        [Display(Name = "Descripción")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "El {0} es requerido.")]
+        [Display(Name = "Monto Pagado")]
         public double PaidAmount { get; set; }
+
+        [Required(ErrorMessage = "El {0} es requerido.")]
+        [Display(Name = "Cambio")]
         public double Change { get; set; }
+
+        [Required(ErrorMessage = "El {0} es requerido.")]
+        [DisplayFormat(DataFormatString="{0:dd/MM/yyyy}", ApplyFormatInEditMode = false)]
+        [Display(Name = "Fecha y Hora")]
         public DateTime DateTime { get; set; }
+
+        [Required(ErrorMessage = "El {0} es requerido.")]
+        [Display(Name = "Id de usuario")]
         public int UserId { get; set; }
+
+        [Required(ErrorMessage = "El {0} es requerido.")]
+        [Display(Name = "Detalle de venta")]
         public int SaleDetailId { get; set; }
+
 
         public CUser User { get; set; }
         public CSaleDetail SaleDetail { get; set; }
--- SistemaPV/Data/Entities/CSaleDetail.cs
+++ SistemaPV/Data/Entities/CSaleDetail.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SistemaPV.Data.Entities
 {
     public class CSaleDetail:IEntity
     {
+        [Display(Name = "Id")]
         public int Id { get; set; }
+
+        [Display(Name = "Total")]
         public double Total { get; set; }
 
         public ICollection<CProduct> Products { get; set; }
