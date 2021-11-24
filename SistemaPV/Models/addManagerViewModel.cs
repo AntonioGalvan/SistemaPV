f3a4024ed33380e404f0869e23c2deef385e2a42
UnclearBag1619 <axel8ravelo@outlook.com>
2021-11-23 18:55:06 -0600

    Agregar Usuarios CManager

---
+++ SistemaPV/Models/addManagerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SistemaPV.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaPV.Models
+{
+    public class addManagerViewModel:CManager
+    {
+
+    }
+}
